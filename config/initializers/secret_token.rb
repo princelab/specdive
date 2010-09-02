cd11f5b9666e95c90ca48340af247b960438a428
Jamison Dance <jergason@gmail.com>
2010-09-02 13:53:19 -0600

    inital commit of specdive

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Specdive::Application.config.secret_token = '39441d0a1d4e836d162522f33d78602bf660e046462f511c2e2a7c11cd96f474dd87c0bb83b07be91aab24023eeca69328c4f2b2965a55cbe647b6e469a9a35a'
